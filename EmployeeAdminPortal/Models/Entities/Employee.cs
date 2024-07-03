9aa18423b957e591b43c2ec36c7ca0ce67974d76
senzonzuza <senzokaqhaka@gmail.com>
2024-07-03 15:54:18 +0200

    model,applicationdb and employee class added

---
+++ EmployeeAdminPortal/Models/Entities/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace EmployeeAdminPortal.Models.Entities
+{
+	public class Employee
+	{
+		public Guid Id { get; set; }
+		public required string Name { get; set; }
+		public required string Email { get; set; }
+		public string? Phone { get; set; }
+		public decimal Salary { get; set; }
+	}
+
+}
+
